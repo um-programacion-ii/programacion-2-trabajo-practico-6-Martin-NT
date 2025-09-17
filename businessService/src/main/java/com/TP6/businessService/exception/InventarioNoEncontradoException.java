d95f6839c80ee52d7785add14caabddb47275a41
Martin-NT <mt.navarro1503@gmail.com>
2025-09-17 19:03:05 -0300

    Bussiness Service

---
+++ businessService/src/main/java/com/TP6/businessService/exception/InventarioNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.TP6.businessService.exception;
+
+public class InventarioNoEncontradoException extends RuntimeException {
+    public InventarioNoEncontradoException(String message) {
+        super(message);
+    }
+}
